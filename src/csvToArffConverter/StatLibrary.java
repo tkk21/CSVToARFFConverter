755299535daa3dc1206bd19aa487a8ea03d4e500
Ted Kim <tkk21@case.edu>
2015-09-26 17:07:22 -0400

    turned into static

--- src/csvToArffConverter/StatLibrary.java
+++ src/csvToArffConverter/StatLibrary.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class StatLibrary {
 
-	public double calculateMean (List<String> column){
+	public static double calculateMean (List<String> column){
 		double sum = 0.0;
 		for (String s : column){
 			try {
@@ -17,12 +17,13 @@ public class StatLibrary {
 		return sum/column.size();
 	}
 	
-	public double calculateStandardDeviation(List<String> column){
+	public static double calculateStandardDeviation(List<String> column){
 		double mean = calculateMean(column);
 		double variance = 0.0;
 		for (String s : column){
 			try {
 				variance = (Double.parseDouble(s) - mean);
+				variance = variance*variance;
 			}
 			catch (NumberFormatException e){
 				System.out.println("bad double value in column");
