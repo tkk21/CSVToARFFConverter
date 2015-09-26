e1fd63d114db4bccde3af4aceb7b915560d0f045
Ted Kim <tkk21@case.edu>
2015-09-26 15:29:56 -0400

    wrapped all the operations needed to convert into one function

--- src/csvToArffConverter/ConvertCSVToARFF.java
+++ src/csvToArffConverter/ConvertCSVToARFF.java
@@ -2,24 +2,53 @@ package csvToArffConverter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConvertCSVToARFF {
 
-	public static void main(String[] args) {
-		ARFFData arff = null;
+	public static List<ARFFData> convert(File fileA, File fileB){
+		List<ARFFData> trainTestPair = new ArrayList<ARFFData>();
+		final int ROWS_FOR_TRAIN = 35;
+		ARFFData arffA = null;
+		ARFFData arffB = null;
 		try {
-			arff = new ARFFData(CSVParser.parseCSV(new File(args[0])));
+			arffA = new ARFFData(CSVParser.parseCSV(fileA));
+			arffB = new ARFFData(CSVParser.parseCSV(fileB));
 		}
 		catch(FileNotFoundException e){
 			System.out.println("the csv file to read does not exist");
 			System.exit(1);
 		}
 		
-		arff.deleteAttribute("TimeStamp");
-		arff.deleteAttribute("Counter");
-		arff.deleteAttribute("mCurrentPattern");
-		arff.insertAttribute("Label", "A");
-		ARFFWriter.convertToARFF(new File(args[1]), arff);//train data
+		arffA.deleteAttribute("TimeStamp");
+		arffA.deleteAttribute("mCurrentPattern");
+		arffA.insertAttribute("Label", "A");
+		
+		arffB.deleteAttribute("TimeStamp");
+		arffB.deleteAttribute("mCurrentPattern");
+		arffB.insertAttribute("Label", "B");
+		
+		ARFFData arffATest = arffA.splitData(ROWS_FOR_TRAIN);
+		ARFFData arffBTest = arffB.splitData(ROWS_FOR_TRAIN);
+		
+		//combine arffA = arffA + arffB
+		arffA.combineData(arffB);
+		arffATest.combineData(arffBTest);
+		
+		arffA.deleteAttribute("Counter");
+		arffATest.deleteAttribute("Counter");
+		trainTestPair.add(arffA);
+		trainTestPair.add(arffATest);
+		
+		return trainTestPair;
+	}
+	
+	public static void main(String[] args) {
+		List<ARFFData> list = convert(new File(args[0]), new File(args[1]));
+		ARFFWriter.convertToARFF(new File("train.arff"), list.get(0));
+		ARFFWriter.convertToARFF(new File("test.arff"), list.get(1));
+		
 		System.out.println("done");
 	}
 }
